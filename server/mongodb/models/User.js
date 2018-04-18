99840ee9a7d4a22f7365c0d7813d9ef7292e0082
kharazzoro <kha.1991.14.2017@gmail.com>
2018-04-18 14:02:08 +0100

    add department mongodb models schema

--- server/mongodb/models/User.js
+++ server/mongodb/models/User.js
@@ -2,7 +2,16 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
 const schema = new Schema({
-    username: String     
+    username: String,
+    firstname:String,
+    lastname:String,
+    username:String,
+    wegimail:String,
+    emailAddress:String,
+    password:String,
+    isAdmin:Boolean,
+    phoneNumber:Boolean ,
+    departmantSym:String   
 });
 
 const Account = mongoose.model('account', schema);
